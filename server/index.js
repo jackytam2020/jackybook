b04fa8e5ad67f7549d9bbb66fb9a884f789d924c
jackytam2020 <jackytam2020@gmail.com>
2023-03-27 13:01:42 -0700

    changed cors code

--- server/index.js
+++ server/index.js
@@ -30,11 +30,7 @@ app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }));
 app.use(morgan('common'));
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
-app.use(
-  cors({
-    origin: 'http://localhost:3000',
-  })
-);
+app.use(cors());
 app.use('/assets', express.static(path.join(__dirname, 'public/assets')));
 
 // FILE STORAGE
