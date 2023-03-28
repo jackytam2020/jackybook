bef178c1a61a01d25063fec6835d90072277c351
jackytam2020 <jackytam2020@gmail.com>
2023-03-28 14:10:06 -0700

    changed env variable name

--- server/config/database.js
+++ server/config/database.js
@@ -4,7 +4,7 @@ mongoose.set('strictQuery', false);
 
 export const connectDB = async () => {
   try {
-    mongoose.connect(process.env.MONGO_URL, {
+    mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
